7f5287fb41a6306a8f21f3cd5dc7aee4a564d2ca
Kevin Downs <kevin.downs04@gmail.com>
2019-11-01 22:27:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-
+  songs.each_with_index do |element, index|
+    if input == element
+      
+  end
     puts "Playing #{songs}"
     puts "Invalid input, please try again"
 end
